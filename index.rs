6e3449b319a9971f0b752d6f391fa1ab340f24b4
Mathspy <mathspy257@gmail.com>
2019-03-13 01:11:53 +0200

    Mad science...

--- index.rs
+++ index.rs
@@ -1,8 +1,58 @@
+use core::fmt::Display;
 use git2::Repository;
 use http::{header, Request, Response, StatusCode};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use tokei::{Config, Languages};
+use tokei::{Config, Language, Languages};
 use url::Url;
+
+#[derive(Serialize, Deserialize)]
+struct Lang {
+    name: String,
+    files: usize,
+    lines: usize,
+    code: usize,
+    comments: usize,
+    blanks: usize,
+}
+impl Lang {
+    fn from_language<S: Display>((name, language): (&S, &Language)) -> Lang {
+        Lang {
+            name: name.to_string(),
+            files: language.stats.len(),
+            lines: language.lines,
+            code: language.code,
+            comments: language.comments,
+            blanks: language.blanks,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Output {
+    languages: Vec<Lang>,
+    total: Lang,
+}
+
+fn make_json(languages: Languages) -> Output {
+    Output {
+        languages: languages
+            .iter()
+            .filter(|(_, language)| !language.is_empty())
+            .map(Lang::from_language)
+            .collect(),
+        total: Lang::from_language((
+            &"Total",
+            &languages
+                .iter()
+                .fold(Language::new(), |mut total, (_, language)| {
+                    // TODO: PLZ NO CLONE
+                    total += language.clone();
+                    total
+                }),
+        )),
+    }
+}
 
 fn handler(request: Request<()>) -> http::Result<Response<String>> {
     let url = Url::parse(&request.uri().to_string()).unwrap();
@@ -12,18 +62,20 @@ fn handler(request: Request<()>) -> http::Result<Response<String>> {
         (Some(user), Some(repo)) => {
             let repo_url = format!("https://github.com/{}/{}", user, repo);
 
-            match Repository::clone(&repo_url, "./repo") {
+            match Repository::clone(&repo_url, "/tmp/repo") {
                 Ok(repo) => repo,
                 Err(e) => panic!("failed to clone: {}", e),
             };
 
             let mut languages = Languages::new();
-            languages.get_statistics(&["./repo"], &[".git"], &Config::default());
+            languages.get_statistics(&["/tmp/repo"], &[".git"], &Config::default());
+
+            let data = make_json(languages);
 
             let response = Response::builder()
                 .status(StatusCode::OK)
                 .header(header::CONTENT_TYPE, "text/plain")
-                .body(serde_json::to_string(&languages).unwrap())
+                .body(serde_json::to_string(&data).unwrap())
                 .expect("failed to render response");
 
             Ok(response)
@@ -35,3 +87,13 @@ fn handler(request: Request<()>) -> http::Result<Response<String>> {
     }
 }
 
+// For local testing:
+// fn main() {
+//     let lol = handler(
+//         Request::get("https://tokei-now-awqpllhtf.now.sh/?user=mathspy&repo=binary-clock")
+//             .body(())
+//             .unwrap(),
+//     );
+
+//     println!("{}", lol.unwrap().body());
+// }
